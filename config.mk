1a1818db8a648cf21882c90c18969ac7b25b04ef
Roman Dayneko <roman.dayneko@gmail.com>
2015-05-18 12:18:18 +0300

    parser have been mooved to main project

--- config.mk
+++ config.mk
@@ -1,12 +1,9 @@
 APP = erleveapi
 erleveapi_DIR = .
 
-DEPS ?= jsx erleveapi_parser
+DEPS ?= jsx 
 
 jsx_SOURCE ?= "git@github.com:talentdeficit/jsx.git"
 jsx_DIR ?= $(DEPSDST)/jsx
 jsx_ERLCFLAGS ?= -Dmaps_support
-
-erleveapi_parser_SOURCE ?= "git@github.com:skolot/erleveapi_parser.git"
-erleveapi_parser_DIR ?= $(DEPSDST)/erleveapi_parser
 
