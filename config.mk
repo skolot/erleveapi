818ac58d26f55ab3233d6946a96760abde4358bc
Roman Dayneko <roman.dayneko@gmail.com>
2015-08-11 11:06:39 +0300

    added dependicies

--- config.mk
+++ config.mk
@@ -1,9 +1,19 @@
 APP = erleveapi
 erleveapi_DIR = .
 
-DEPS ?= jsx 
+DEPS ?= jsx cowlib ranch cowboy
 
 jsx_SOURCE ?= "git@github.com:talentdeficit/jsx.git"
 jsx_DIR ?= $(DEPSDST)/jsx
 jsx_ERLCFLAGS ?= -Dmaps_support
 
+cowlib_SOURCE ?= "git@github.com:ninenines/cowlib.git"
+cowlib_DIR ?= $(DEPSDST)/cowlib
+cowlib_GIT_CLONE_OPT ?= --branch 1.3.0
+
+ranch_SOURCE ?= "git@github.com:ninenines/ranch.git"
+ranch_DIR ?= $(DEPSDST)/ranch
+ranch_GIT_CLONE_OPT ?= --branch 1.0.0
+
+cowboy_SOURCE ?= "git@github.com:ninenines/cowboy.git"
+cowboy_DIR ?= $(DEPSDST)/cowboy
